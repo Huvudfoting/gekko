49ad2acb9c8741d7d8afa389bd9f7b332a9f79b8
Daniel Routovaara <daniel.routovaara@gmail.com>
2018-01-28 20:03:10 +0100

    changed startup scripts to be run from local folder and log to /logs

--- start-LIVE-kraken-XBT.sh
+++ start-LIVE-kraken-XBT.sh
@@ -1,4 +1,4 @@
-stdbuf -o 0 /usr/bin/node /home/pi/gekko/gekko --config 
+stdbuf -o 0 node gekko --config 
 kraken-LIVE-XBTEUR.config.js 
-2>&1 | tee /home/pi/gekko-logs/live-XBTEUR-kraken.log
+2>&1 | tee logs/live-XBTEUR-kraken.log
 
--- start-PAPER-kraken-XBT.sh
+++ start-PAPER-kraken-XBT.sh
@@ -1,4 +1,4 @@
-stdbuf -o 0 /usr/bin/node /home/pi/gekko/gekko --config 
+stdbuf -o 0 node gekko --config 
 kraken-PAPER-XBTEUR.config.js 2>&1 | 
-tee /home/pi/gekko-logs/paper-trading-kraken.log
+tee logs/paper-trading-kraken.log
 
